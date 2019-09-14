b6c14ccad7cd1ae3f966ef82373596462d22df28
Pleshw <pleshw@outlook.com>
2019-09-14 03:13:37 -0300

    Update GameManager.ts

--- src/includes/GameManager.ts
+++ src/includes/GameManager.ts
@@ -2,17 +2,32 @@ import { Player } from '@/includes/Player'
 
 class GameManager {
     players!: Map<number, Player>;
+
+    /// Um modulo é um aglomerado de questões de várias categorias com um tema em comum
+    modules: Array<Module> = new Array<Module>();
+
+    /// Uma tarefa é algo a ser feito no jogo, e que possui
+    /// informações sobre como essa tarefa deve ser feita
+    tasks: Array<String> = new Array<Task>();
+
     materials: Array<String> = new Array<String>();
-    questions: Array<String> = new Array<Question>();
+
+    rounds: Stack<Rounds> = new Stack<Rounds>();
 
     public addMaterial(material: Array<string>): void {
         this.materials = [...material];
     }
 
     public StartGame(): void {
-        this.prepareQuestionsWithAvailableMaterials();
-
+        // prepara todas as tarefas dos módulos
+        this.getTasksFromModules();
+        // filtra tarefas com os materiais disponiveis
+        this.prepareAvailableTasks();
+        // divide as tarefas preparando as rodadas do jogo
+        this.setupRounds();
     }
+
+    public getRandomAvailableQuestion(): task;
 
 }
 
